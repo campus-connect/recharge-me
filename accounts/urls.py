12e106579b017fd73b9627b07d6808ce662e3343
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-06-21 19:44:35 +0100

    demo view

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+#  * This file is part of recharge-me project.
+#  * (c) Ochui Princewill Patrick <ochui.princewill@gmail.com>
+#  * For the full copyright and license information, please view the "LICENSE.md"
+#  * file that was distributed with this source code.
+
+from django.urls import path
+from .views import Dashboard
+
+urlpatterns = [
+    path("", Dashboard.as_view(), name="dashboard")
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+#  * This file is part of recharge-me project.
+#  * (c) Ochui Princewill Patrick <ochui.princewill@gmail.com>
+#  * For the full copyright and license information, please view the "LICENSE.md"
+#  * file that was distributed with this source code.
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
+from allauth.account.models import EmailAddress
+
+class Dashboard(LoginRequiredMixin, TemplateView):
+    
+    template_name = 'dashboard/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #check user email status
+        if not EmailAddress.objects.filter(user=self.request.user, verified=True).exists():
+            context['verified_email'] = False
+        else:
+            context['verified_email'] = True
+                
+        return context
