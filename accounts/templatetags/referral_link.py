ed4320ac88476f69d68732533529c02014b1dfbc
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-07-06 22:30:43 +0100

    fix referral link

--- accounts/templatetags/referral_link.py
+++ accounts/templatetags/referral_link.py
@@ -7,33 +7,37 @@ from django import template
 from django.urls import reverse
 from django.core.cache import cache
 from referrals.models import Link
+from allauth.account.models import EmailAddress
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def referral_link(context):
-    
+
     request = context.get('request', None)
     if request:
         address = request.build_absolute_uri(reverse('account_signup'))
         if request.user.is_authenticated:
-            user = context['request'].user
-            
-            # Reduce database hits
-            if '{}_referral_link'.format(user) in cache:
-                token = cache.get('{}_referral_link'.format(user))
+            if EmailAddress.objects.filter(user=request.user, verified=True).exists():
+                user = context['request'].user
+
+                # Reduce database hits
+                if '{}_referral_link'.format(user) in cache:
+                    token = cache.get('{}_referral_link'.format(user))
+                else:
+                    try:
+                        link = Link.objects.get(user=user)
+                        token = link.token
+                        cache.set(
+                            '{}_referral_link'.format(user),
+                            '{}'.format(token),
+                            60*60*24
+                        )
+                    except Link.DoesNotExist:
+                        return 'Token not found'
+
+                    return '{}?&ref={}'.format(address, token)
+
             else:
-                try:
-                    link = Link.objects.get(user=user)
-                    token = link.token
-                    cache.set(
-                        '{}_referral_link'.format(user),
-                        '{}'.format(token),
-                        60*60*24
-                    )
-                except Link.DoesNotExist:
-                    pass
-
-            return '{}?&ref={}'.format(address, token)
-
-        return 'Please verify your E-mail'
+                return 'Please verify your E-mail'
