4eefe6eda6c58566d53c82eed6cbe1dc1b093099
ochui princewill patrick <ochuiprincewill411@gmail.com>
2019-08-10 01:34:52 +0100

    allow all host

--- recharge/settings.py
+++ recharge/settings.py
@@ -27,10 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True' # Python does not parse environment variables to Python objects, it just gets them as strings. 
 
-ALLOWED_HOSTS = ['agapeer.me']
-
-if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
